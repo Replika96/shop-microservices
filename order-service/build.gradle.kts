4837ee278eae900e6abcdbb96d366c1d0bb3cc6f
Replika96 <tazmin.vadim31@gmail.com>
2026-04-27 17:38:04 +0300

    "Fix Jackson serialization for Kotlin and LocalDateTime"

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     runtimeOnly("org.postgresql:postgresql")
